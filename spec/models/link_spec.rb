d8109ec77e977c233d74e21dbaf3f982eadfe1f2
Loumarven Payot <lou.payot@gmail.com>
2020-05-05 15:18:20 +0800

    Add test for link model methods

---
+++ spec/models/link_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe Link, "#upvote" do
+  it "increments upvotes" do
+    link = build(:link, upvotes: 1)
+
+    link.upvote
+
+    expect(link.upvotes).to eq 2
+  end
+end
+
+RSpec.describe Link, "#downvote" do
+  it "increments downvotes" do
+    link = build(:link, downvotes: 1)
+
+    link.downvote
+
+    expect(link.downvotes).to eq 2
+  end
+end
+
+RSpec.describe Link, "#score" do
+  it "returns the upvotes minus the downvotes" do
+    link = Link.new(upvotes: 2, downvotes: 1)
+
+    expect(link.score).to eq 1
+  end
+end
+
+RSpec.describe Link, ".hottest_first" do
+  it "returns the links: hottest to coldest" do
+    coldest_link = create(:link, upvotes: 3, downvotes: 3)
+    hottest_link = create(:link, upvotes: 5, downvotes: 1)
+    lukewarm_link  = create(:link, upvotes: 2, downvotes: 1)
+
+    expect(Link.hottest_first).to eq [hottest_link, lukewarm_link, coldest_link]
+  end
+end
